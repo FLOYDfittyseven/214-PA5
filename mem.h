e952f04fd97f5cb9bef7361379235f398f1a403a
FLOYDfittyseven <FLOYDfittyseven@users.noreply.github.com>
2015-04-30 18:58:23 -0400

    Create mem.h

---
+++ mem.h
@@ -0,0 +1,15 @@
+struct memEntry{
+   struct memEntry *prev, *next;
+   int isfree;
+   unsigned int size;
+}
+
+void *
+mymalloc( unsigned int size, char * file, int line );
+
+void
+myfree( void * p, char * file, int line );
+
+#define blocksize(1024*1024)
+#define malloc( x )	mymalloc( x, __FILE__, __LINE__ )
+#define free( x ) myfree( x, __FILE__, __LINE__ )
