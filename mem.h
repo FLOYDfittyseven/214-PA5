ea9b0371cf1b2a53ce9b6f6a03615ce719b1c925
FLOYDfittyseven <FLOYDfittyseven@users.noreply.github.com>
2015-05-01 23:19:28 -0400

    Update mem.h

--- mem.h
+++ mem.h
@@ -3,7 +3,7 @@ struct memEntry{
    int isfree;
    unsigned int size;
    unsigned int sanity_check;
-}
+};
 
 void
 heapTraversal();
@@ -11,12 +11,9 @@ heapTraversal();
 void *
 mymalloc( unsigned int size, char * file, int line );
 
-void *
-mymalloc2( unsigned int size, char * file, int line );
-
 void
 myfree( void * p, char * file, int line );
 
-#define blocksize(1024*1024)
+#define blocksize (1024*1024)
 #define malloc( x )	mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
